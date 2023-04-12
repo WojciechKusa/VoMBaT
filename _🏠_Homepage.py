009248af55882ba9db5402c56f688738b37aab4e
Wojciech Kusa <wojciech.kusa@gmail.com>
2023-04-12 20:24:53 +0200

    feat: update text on homepage

--- _🏠_Homepage.py
+++ _🏠_Homepage.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Comparison of evaluation measures for High-Recall Search Tasks")
+st.title("VoMBaT🐻 - Visualisation of Evaluation Measure Behaviour for High-Recall Search Tasks")
 
 st.write(
     """
@@ -23,13 +23,18 @@ st.write(
     
     ________
     
-    For each of these pages a set of predefined systematic review datasets was prepared. 
+    For each of these pages a set of predefined systematic review datasets' parameters was prepared. 
     You can select one of these datasets and then select the evaluation measures you want to compare.
     You can also define custom dataset by selecting it's size and the percentage of relevant documents (includes). 
 
-    There are two types of predefined datasets:
-    - three synthetic datasets showing extreme options for the distribution of relevant documents (includes) in the dataset
-    - fifteen real datasets from systematic reviews in the field of medicine introduced in \
+    There are two types of predefined datasets' parameters:
+    - Three synthetic examples of dataset parameters showing extreme options for the distribution of relevant \
+    documents ($\mathcal{I}$) in the dataset: 
+        - balanced, 
+        - heavily unbalanced towards positive class (example of a very good search query), 
+        - heavily unbalanced towards negative class (very typical in systematic reviews).
+    - Fifteen datasets which use the $N$ and $\mathcal{I}$ values from systematic reviews \
+    in the field of medicine introduced in \
     *Cohen, A. M., Hersh, W. R., Peterson, K., & Yen, P. Y. (2006). \
     Reducing workload in systematic review preparation using automated citation classification. \
     Journal of the American Medical Informatics Association, 13(2), 206-219.*
